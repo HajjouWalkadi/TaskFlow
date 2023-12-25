fb8359e09ebf1a3d526d431c03627adfc8c7fc88
hajjou walkadi <walkadihajjou153@gmail.com>
2023-12-25 14:40:39 +0100

    Saving Task is added in TaskController

--- src/main/java/com/example/taskflow/controller/TaskController.java
+++ src/main/java/com/example/taskflow/controller/TaskController.java
@@ -3,11 +3,14 @@ package com.example.taskflow.controller;
 import com.example.taskflow.dto.TaskRequestDTO;
 import com.example.taskflow.entities.Task;
 import com.example.taskflow.handler.response.ResponseMessage;
+import com.example.taskflow.mappers.TaskMapper;
 import com.example.taskflow.service.TaskService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 import static org.apache.logging.log4j.ThreadContext.isEmpty;
@@ -38,7 +41,7 @@ public class TaskController {
         return ResponseMessage.ok("Success", null);
     }
 
-    @PostMapping("/save")
+  /* @PostMapping("/save")
     public ResponseEntity addTask(@Valid @RequestBody TaskRequestDTO taskRequestDTO)  {
         Task task1 = taskService.createTask(taskRequestDTO.toTask());
         if (task1 == null) {
@@ -46,17 +49,34 @@ public class TaskController {
         }else {
             return ResponseMessage.created("Success", task1);
         }
-    }
+    }*/
 
-    @PutMapping("/{id}")
+   @PostMapping("/save")
+   public ResponseEntity createTask(@RequestBody @Valid TaskRequestDTO taskRequestDTO) {
+       LocalDateTime now = LocalDateTime.now();
+       LocalDateTime endDate = taskRequestDTO.getEndDate();
+
+       if (endDate != null && now.until(endDate, ChronoUnit.DAYS) > 3) {
+           return ResponseMessage.badRequest("Task deadline cannot be more than 3 days in advance");
+       }
+       Task task = TaskMapper.mapTaskDtoToTask(taskRequestDTO);
+       Task task1 = taskService.createTask(task);
+       if (task1 == null) {
+           return ResponseMessage.badRequest("Failed To Create Task");
+       } else {
+           return ResponseMessage.created("Task Created Successfully", task1);
+       }
+   }
+
+    /*@PutMapping("/{id}")
     public ResponseEntity updateTask(@RequestBody TaskRequestDTO taskRequestDTO, @PathVariable Long id) {
-        Task task = taskService.updateTask(taskRequestDTO.toTask(), id);
+        Task task = taskService.updateTask(taskRequestDTO, id);
         if(task == null) {
             return ResponseMessage.badRequest("Task not updated");
         }else {
             return ResponseMessage.created("Success", task);
         }
-    }
+    }*/
 
     @DeleteMapping("/{id}")
     public ResponseEntity deleteTask(@PathVariable Long id) {
