056db9b427934aae6fb058bdb41c9b2880575fc8
hajjou walkadi <walkadihajjou153@gmail.com>
2023-12-25 14:35:31 +0100

    Adding validation in Task Request DTO

--- src/main/java/com/example/taskflow/dto/TaskRequestDTO.java
+++ src/main/java/com/example/taskflow/dto/TaskRequestDTO.java
@@ -1,22 +1,62 @@
 package com.example.taskflow.dto;
 
+import com.example.taskflow.entities.Tags;
 import com.example.taskflow.entities.Task;
+import com.example.taskflow.entities.User;
+import com.example.taskflow.enums.TaskActionType;
 import com.example.taskflow.enums.TaskStatus;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-public record TaskRequestDTO (
-        String title,
-        String description,
-        String status,
-        Long tagId
-) {
- public Task toTask() {
-     Task.TaskBuilder taskBuilder = new Task().builder()
-                .title(title)
-                .description(description);
-        if(status != null) {
-            taskBuilder.status(TaskStatus.valueOf(status));
-        }
-        return taskBuilder.build();
-    }
+import java.time.LocalDateTime;
+import java.util.List;
 
- }
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TaskRequestDTO {
+
+
+
+    @NotBlank(message = "Title is required")
+    private String title;
+
+    @NotBlank(message = "Description is required")
+    private String description;
+
+    @NotNull(message = "Completed status is required")
+    private Boolean completed;
+
+    @NotNull(message = "Start date of task cannot be null")
+    @FutureOrPresent(message = "Start date must be in the present or future")
+    private LocalDateTime startDate;
+
+    @NotNull(message = "End date of task cannot be null")
+    private LocalDateTime endDate;
+    private LocalDateTime modificationDate;
+
+    @NotNull(message = "At least one status is required")
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status;
+
+    @Enumerated(EnumType.STRING)
+    private TaskActionType actionType;
+
+    @NotEmpty(message = "Tag is required")
+    @Size(min = 3, message = "At least three tags are required")
+    private List<Tags> tags;
+
+    @NotNull(message = "CreatedBy is required")
+    private Long createdById;
+
+    @NotNull(message = "AssignedTo is required")
+    private Long assignedToId;
+}
+
