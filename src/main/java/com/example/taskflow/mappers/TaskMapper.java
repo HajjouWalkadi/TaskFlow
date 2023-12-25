d601597df47183e304fcb9a8f0926d30608fecd2
hajjou walkadi <walkadihajjou153@gmail.com>
2023-12-25 14:38:59 +0100

    TaskMapper is well added

--- src/main/java/com/example/taskflow/mappers/TaskMapper.java
+++ src/main/java/com/example/taskflow/mappers/TaskMapper.java
@@ -1,2 +1,22 @@
-package com.example.taskflow.mappers;public class TaskMapper {
+package com.example.taskflow.mappers;
+
+import com.example.taskflow.dto.TaskRequestDTO;
+import com.example.taskflow.entities.Task;
+
+public class TaskMapper {
+    public static Task mapTaskDtoToTask(TaskRequestDTO taskRequestDTO) {
+        return Task.builder()
+                .title(taskRequestDTO.getTitle())
+                .description(taskRequestDTO.getDescription())
+                .completed(taskRequestDTO.getCompleted())
+                .startDate(taskRequestDTO.getStartDate())
+                .endDate(taskRequestDTO.getEndDate())
+                .modificationDate(taskRequestDTO.getModificationDate())
+                .status(taskRequestDTO.getStatus())
+                .actionType(taskRequestDTO.getActionType())
+                .tags(taskRequestDTO.getTags())
+                //.assignedTo(taskRequestDTO.getAssignedTo().getId())
+                //.createdBy(taskRequestDTO.getCreatedBy().getId())
+                .build();
+    }
 }
