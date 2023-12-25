28e6bbb7fcfc841ee24c0684698e3449ca01d8a8
hajjou walkadi <walkadihajjou153@gmail.com>
2023-12-25 14:39:48 +0100

    GlobalHandlerException is well added

--- src/main/java/com/example/taskflow/handler/GlobalHandlerExceptions.java
+++ src/main/java/com/example/taskflow/handler/GlobalHandlerExceptions.java
@@ -1,2 +1,27 @@
-package com.example.taskflow.handler;public class GlobalHandlerExceptions {
+package com.example.taskflow.handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalHandlerExceptions {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            String fieldName = error.getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return ResponseEntity.badRequest().body(errors);
+    }
 }
